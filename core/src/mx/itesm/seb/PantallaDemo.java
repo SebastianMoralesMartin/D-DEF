4dca1916d5b2ca6cee9e6078e9d21c6c0a324763
RaulOrtizMateos <A01375407@itesm.mx>
2019-09-23 10:14:35 -0500

    prueba raul

--- core/src/mx/itesm/seb/PantallaDemo.java
+++ core/src/mx/itesm/seb/PantallaDemo.java
@@ -17,7 +17,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-
+//Raul
 class PantallaDemo implements Screen {
     //Variables iniciales
     private final Juego juego;

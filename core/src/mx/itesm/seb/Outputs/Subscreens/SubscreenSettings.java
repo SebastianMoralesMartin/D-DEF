65e10b71b70800b9ce08c2085260ce6cbb369ec0
Alberto López <pipe.at.developer@gmail.com>
2019-11-27 17:11:41 -0600

    Alpha 1.6.8
    Inserción de botón Delete Data en Subscreen Settings.

--- core/src/mx/itesm/seb/Outputs/Subscreens/SubscreenSettings.java
+++ core/src/mx/itesm/seb/Outputs/Subscreens/SubscreenSettings.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.utils.Align;
 
+import mx.itesm.seb.Inputs.Buttons.ButtonDeleteData;
 import mx.itesm.seb.Inputs.Buttons.ButtonToMain;
 import mx.itesm.seb.Inputs.CheckBoxes.SettingSwitch;
 import mx.itesm.seb.Inputs.TextFields.InsertNameTextField;
@@ -21,6 +22,7 @@ public class SubscreenSettings {
     private SettingSwitch switchDarkMode;
     private SettingSwitch switchMusic;
     private SettingSwitch switchSound;
+    private ButtonDeleteData buttonDeleteData;
     private Label name;
     private Label darkMode;
     private Label music;
@@ -76,6 +78,7 @@ public class SubscreenSettings {
     }
 
     private void setButtons() {
+        this.buttonDeleteData = new ButtonDeleteData(this.videogame, this.uiButton);
         this.buttonToMain = new ButtonToMain(this.videogame, (EnhancedScreen) this.videogame.getScreen(), this.uiButton);
     }
 
@@ -87,7 +90,7 @@ public class SubscreenSettings {
     private void setWindowElements() {
         this.window.add(this.name).pad(10).colspan(3);
         this.window.row();
-        this.window.add(this.insertNameTextField).pad(10).colspan(3).fillX();
+        this.window.add(this.insertNameTextField).pad(10).padBottom(20).colspan(3).fillX();
         this.window.row();
         this.window.add(this.switchDarkMode).pad(10).fillX();
         this.window.add(this.darkMode).pad(10).fillX();
@@ -98,7 +101,9 @@ public class SubscreenSettings {
         this.window.add(this.switchSound).pad(10).fillX();
         this.window.add(this.sound).pad(10).fillX();
         this.window.row();
-        this.window.add(this.buttonToMain).pad(10).colspan(2);
+        this.window.add(this.buttonDeleteData).width(600).height(150).pad(20).colspan(3).fillX();
+        this.window.row();
+        this.window.add(this.buttonToMain).pad(10).colspan(3);
     }
 
     private void setWindowProperties() {

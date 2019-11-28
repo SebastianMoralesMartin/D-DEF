7a0f7608c7d8d6106dc197f6fbb57373ec42adc5
Edumu <a01745219@itesm.mx>
2019-11-28 09:59:59 -0600

    Bordes del submarino

--- core/src/mx/itesm/seb/Outputs/Screens/ScreenSurvive.java
+++ core/src/mx/itesm/seb/Outputs/Screens/ScreenSurvive.java
@@ -481,14 +481,19 @@ public class ScreenSurvive extends EnhancedScreen implements Screen {
     }
 
     private void updateSubmarine() {
+
         switch(stateSubmarine){
             case RIGHT:
+                if(playerSubmarine.getSprite().getX() + playerSubmarine.getSprite().getWidth() < (Videogame.WIDTH)){
                 playerSubmarine.move(3, 0);
                 energy -=.05f;
+                break;}
                 break;
             case LEFT:
+                if(playerSubmarine.getSprite().getX() > 0){
                 playerSubmarine.move(-3, 0);
                 energy-=.05f;
+                break;}
                 break;
         }
         if (energy <= 0) {

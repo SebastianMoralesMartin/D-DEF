8555f4c93950037629d7db435fde0b8f57da4078
Alberto López <pipe.at.developer@gmail.com>
2019-11-28 05:35:31 -0600

    Alpha 1.9
    Actualización del GUI en Screen Survive. Pausa todavía no funciona.

--- core/src/mx/itesm/seb/Outputs/Screens/ScreenSurvive.java
+++ core/src/mx/itesm/seb/Outputs/Screens/ScreenSurvive.java
@@ -12,13 +12,22 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -38,8 +47,10 @@ import mx.itesm.seb.Videogame;
 
 import static java.lang.Math.abs;
 
-public class ScreenSurvive implements Screen {
+public class ScreenSurvive extends EnhancedScreen implements Screen {
     private final Videogame videogame;
+    private Skin uiButton;
+    private Skin uiSkin;
     private OrthographicCamera camera;
     private Viewport view;
     private SpriteBatch batch;
@@ -51,7 +62,7 @@ public class ScreenSurvive implements Screen {
     private float MAX_STEP = 0.075f;
     private PlayerSubmarine playerSubmarine;
     private Movement stateSubmarine = Movement.STATIC;
-    private Texture textureBackground;
+    private Image imageBackground;
     private Stage survive;
     private int energy = 300;
     private Text text;
@@ -69,6 +80,16 @@ public class ScreenSurvive implements Screen {
     private Texture healthbarForeGround, healthbarBackGround;
     private PauseScene pauseScene;
 
+    private Table bottomLayout;
+    private Table topBottomLayout;
+    private ImageButton btnBack;
+    private TextButton btnDer;
+    private TextButton btnIzq;
+    private TextButton btnFire;
+    private Button btnPause;
+    private Label labelEnergy;
+    private Label labelScore;
+
     //Music
     private Music backgroundMusic;
 
@@ -83,6 +104,7 @@ public class ScreenSurvive implements Screen {
 
     @Override
     public void show() {
+        this.setSkins();
         setView();
         setTextures();
         setStage();
@@ -96,6 +118,27 @@ public class ScreenSurvive implements Screen {
 
         //PROTOTYPE: Creates an object that draws text outputs onto a screen
         text = new Text("Energy");
+    }
+
+    @Override
+    public void updateScreen() {
+        this.show();
+    }
+
+    public void setSkins() {
+        if(this.videogame.getSettings().getDarkMode() == false) {
+            this.imageBackground = new Image(new Texture(Gdx.files.internal("Skins/Light/lightBackground.png")));
+            this.uiButton = new Skin(Gdx.files.internal("Skins/Buttons/uiButton.json"),
+                    new TextureAtlas(Gdx.files.internal("Skins/Buttons/uiButton.atlas")));
+            this.uiSkin = new Skin(Gdx.files.internal("Skins/Light/uiLightMode.json"),
+                    new TextureAtlas(Gdx.files.internal("Skins/Light/uiLightMode.atlas")));
+        } else {
+            this.imageBackground = new Image(new Texture(Gdx.files.internal("Skins/Dark/darkBackground.png")));
+            this.uiButton = new Skin(Gdx.files.internal("Skins/Buttons/uiButton.json"),
+                    new TextureAtlas(Gdx.files.internal("Skins/Buttons/uiButton.atlas")));
+            this.uiSkin = new Skin(Gdx.files.internal("Skins/Dark/uiDarkMode.json"),
+                    new TextureAtlas(Gdx.files.internal("Skins/Dark/uiDarkMode.atlas")));
+        }
     }
 
     private void setMusic(){
@@ -124,7 +167,7 @@ public class ScreenSurvive implements Screen {
         Texture textureSubmarine = new Texture("Entities/Player/AAPlayer.png");
         LinkedList<Texture> textures = new LinkedList<Texture>();
         textures.add(textureSubmarine);
-        playerSubmarine = new PlayerSubmarine(textures, Videogame.WIDTH /2, 3* Videogame.HEIGHT /7);
+        playerSubmarine = new PlayerSubmarine(textures, Videogame.WIDTH /2, 8 * Videogame.HEIGHT /20);
     }
 
     private void createEnemies() {
@@ -145,12 +188,21 @@ public class ScreenSurvive implements Screen {
 
     private void setStage() {
         survive = new Stage(view);
-        ImageButton btnBack = configurarBotonBack();
-        ImageButton btnDer = configurarBotonDerecha();
-        ImageButton btnIzq = configurarBotonIzquierda();
-        ImageButton btnFire = configureFireButton();
-        ImageButton btnPause = configurePauseButton();
-        addButtons( btnDer, btnIzq, btnFire, btnPause);
+        btnBack = configurarBotonBack();
+        btnDer = configurarBotonDerecha();
+        btnIzq = configurarBotonIzquierda();
+        btnFire = configureFireButton();
+        btnPause = configurePauseButton();
+        labelEnergy = new Label("Energy", this.uiSkin, "default-bold");
+        labelScore = new Label("Score", this.uiSkin, "default-bold");
+        bottomLayout = new Table();
+        bottomLayout.setFillParent(true);
+        bottomLayout.bottom();
+        topBottomLayout = new Table();
+        topBottomLayout.setFillParent(true);
+        topBottomLayout.bottom();
+        addButtons();
+        survive.addActor(bottomLayout);
         if(state == gamestate.PAUSE){addPauseBtn(btnBack);}
 
         Gdx.input.setInputProcessor(survive);
@@ -160,18 +212,22 @@ public class ScreenSurvive implements Screen {
         survive.addActor(btnBack);
     }
 
-    private void addButtons( ImageButton btnRight, ImageButton btnLeft, ImageButton btnFire, ImageButton btnPause) {
+    private void addButtons() {
         if(state == gamestate.GAME){
-            survive.addActor(btnRight);
-            survive.addActor(btnLeft);
-            survive.addActor(btnFire);
-            survive.addActor(btnPause);
+            topBottomLayout.add(labelEnergy).width(340).height(50).pad(20).padBottom(5).padRight(5);
+            topBottomLayout.add(labelScore).width(340).height(50).pad(20).padBottom(5).padLeft(5);
+            topBottomLayout.row();
+            topBottomLayout.add(btnIzq).width(340).height(170).pad(20).padTop(5).padBottom(5).padRight(5);
+            topBottomLayout.add(btnDer).width(340).height(170).pad(20).padTop(5).padBottom(5).padLeft(5);
+            bottomLayout.add(topBottomLayout).colspan(2);
+            bottomLayout.row();
+            bottomLayout.add(btnFire).width(490).height(200).pad(20).padTop(5).padRight(5);
+            bottomLayout.add(btnPause).pad(20).padTop(5).padLeft(5);
         }
-
     }
-    private ImageButton configurePauseButton(){
-        TextureRegionDrawable trdPause = new TextureRegionDrawable(new TextureRegion(new Texture("Buttons/NewStyle/buttonPause.png")));
-        final ImageButton btnPause = new ImageButton(trdPause);
+
+    private Button configurePauseButton(){
+        final Button btnPause = new Button(this.uiButton, "pause");
         btnPause.setPosition(0, Videogame.HEIGHT-btnPause.getHeight());
         btnPause.addListener(new ClickListener(){
             @Override
@@ -192,16 +248,14 @@ public class ScreenSurvive implements Screen {
 
         return btnPause;
     }
-    private ImageButton configureFireButton(){
-        TextureRegionDrawable trdDisparo = new TextureRegionDrawable(
-                new TextureRegion(new Texture("Buttons/buttonBase.png")));
-        final ImageButton btnFire = new ImageButton(trdDisparo);
-        btnFire.setPosition(Videogame.WIDTH - btnFire.getWidth() - 50, 50);
+
+    private TextButton configureFireButton(){
+        final TextButton btnFire = new TextButton("Fire!", this.uiButton, "yellowRectangle");
+        btnFire.getLabel().setFontScale(2f);
         btnFire.addListener(new ClickListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 startTime = TimeUtils.nanoTime();
-
                 return true;
             }
             @Override
@@ -213,27 +267,27 @@ public class ScreenSurvive implements Screen {
                 textures.add(MAX_PROJECTILE_TEXTURE);
                 if(playerProjectile == null) {
                     if (TimeUtils.timeSinceNanos(startTime) < TimeUtils.millisToNanos(500)) {
-                        btnFire.setColor(Color.RED);
+                        btnFire.setColor(Color.YELLOW);
                         speed = 300;
                         power = 25f;
                         playerProjectile = new PlayerProjectile(textures, playerSubmarine);
                         playerProjectile.setType(Projectile.ProjectileType.HIGH);
-                        playeffect();
+                        playEffect();
                         energy-= 25;
                             //startTime = 0;
                     } else if (TimeUtils.timeSinceNanos(startTime) < TimeUtils.millisToNanos(1000)) {
                         //Fire interaction
-                        btnFire.setColor(Color.BLUE);
+                        btnFire.setColor(Color.WHITE);
                         speed = 200;
                         power = 75f;
                         playerProjectile = new PlayerProjectile(textures, playerSubmarine);
                         playerProjectile.setType(Projectile.ProjectileType.MID);
                         playerProjectile.switchTexture();
-                        playeffect();
+                        playEffect();
                         energy -= 75f;
                         //startTime = 0;
                     } else{
-                        btnFire.setColor(Color.PURPLE);
+                        btnFire.setColor(Color.GREEN);
                         speed = 150;
                         power = 100f;
                         //Fire interaction
@@ -241,7 +295,7 @@ public class ScreenSurvive implements Screen {
                         playerProjectile.setType(Projectile.ProjectileType.HIGH);
                         playerProjectile.switchTexture();
                         playerProjectile.switchTexture();
-                        playeffect();
+                        playEffect();
                         energy -= 100f;
                         startTime = 0;
                     }
@@ -252,7 +306,7 @@ public class ScreenSurvive implements Screen {
         return btnFire;
     }
 
-    private void playeffect() {
+    private void playEffect() {
         AssetManager manager = videogame.callAssetManager();
         manager.load("Music/explosion.mp3", Music.class);
         manager.finishLoading();
@@ -261,11 +315,10 @@ public class ScreenSurvive implements Screen {
         effect.play();
     }
 
-    private ImageButton configurarBotonIzquierda() {
+    private TextButton configurarBotonIzquierda() {
         //Botón Izquierda
-        TextureRegionDrawable trdIzq = new TextureRegionDrawable(new TextureRegion(new Texture("Buttons/flechaIzquierda.png")));
-        ImageButton btnIzq = new ImageButton(trdIzq);
-        btnIzq.setPosition(0, 0);
+        btnIzq = new TextButton("< Left", this.uiButton, "orangeRectangle");
+        btnIzq.getLabel().setFontScale(1.5f);
         //Evento de Botón Izquierda
         btnIzq.addListener(new ClickListener(){
             @Override
@@ -287,11 +340,10 @@ public class ScreenSurvive implements Screen {
         return btnIzq;
     }
 
-    private ImageButton configurarBotonDerecha() {
+    private TextButton configurarBotonDerecha() {
         //Botón Derecha
-        TextureRegionDrawable trdDer = new TextureRegionDrawable(new TextureRegion(new Texture("Buttons/flechaDerecha.png")));
-        ImageButton btnDer = new ImageButton(trdDer);
-        btnDer.setPosition(btnDer.getWidth() + 10, 0);
+        btnDer = new TextButton("Right >", this.uiButton, "orangeRectangle");
+        btnDer.getLabel().setFontScale(1.5f);
         //Evento de Botón Derecha
         btnDer.addListener(new ClickListener(){
             @Override
@@ -326,7 +378,6 @@ public class ScreenSurvive implements Screen {
     }
 
     private void setTextures() {
-        textureBackground = new Texture("Screens/Backgrounds/oceanBackgroundPlayVertical_try.png");
         LOW_PROJECTILE_TEXTURE = new Texture("Entities/Projectiles/fireball.png");
         MID_PROJECTILE_TEXTURE = new Texture("Entities/Projectiles/fireball2.png");
         MAX_PROJECTILE_TEXTURE = new Texture("Entities/Projectiles/fireball3.png");
@@ -386,7 +437,7 @@ public class ScreenSurvive implements Screen {
             if(projectileRect.overlaps(enemyRect)){
                 playerProjectile = null;
                 enemies.removeIndex(i);
-                playeffect();
+                playEffect();
                 energy += power *1.25f;
                 if (energy > 300){energy = 300;}
                 destroyed ++;
@@ -493,14 +544,14 @@ public class ScreenSurvive implements Screen {
         drawBackground();
         drawEnemies();
         drawSubmarine();
-        batch.draw(healthbarBackGround, Videogame.WIDTH/2 - 150, 20 * (Videogame.HEIGHT/100)+text.getHeight(), 300, 25);
-        batch.draw(healthbarForeGround, Videogame.WIDTH/2 - 150, 20 * (Videogame.HEIGHT/100)+text.getHeight(), energy, 25);
+        batch.draw(healthbarBackGround, Videogame.WIDTH/2 - 300, 35 * (Videogame.HEIGHT/100)+text.getHeight(), 300 * 2, 25);
+        batch.draw(healthbarForeGround, Videogame.WIDTH/2 - 300, 35 * (Videogame.HEIGHT/100)+text.getHeight(), energy * 2, 25);
         if(playerProjectile != null){
             playerProjectile.render(batch);}
-        text.setMessage("Energy: " + Integer.toString(energy));
-        text.draw(batch, (60 * Videogame.WIDTH)/100, 30 * (Videogame.HEIGHT/100)+text.getHeight());
-        text.setMessage("Score: " + Integer.toString(destroyed));
-        text.draw(batch, (10 * Videogame.WIDTH)/100, 30 * (Videogame.HEIGHT/100)+text.getHeight());
+        labelEnergy.setText("Energy: " + Integer.toString(energy));
+        //text.draw(batch, (60 * Videogame.WIDTH)/100, 35 * (Videogame.HEIGHT/100)+text.getHeight());
+        labelScore.setText("Score: " + Integer.toString(destroyed));
+        //text.draw(batch, (10 * Videogame.WIDTH)/100, 35 * (Videogame.HEIGHT/100)+text.getHeight());
         batch.end();
     }
 
@@ -519,7 +570,7 @@ public class ScreenSurvive implements Screen {
 
     private void drawBackground() {
         //Dibujo de Fondo
-        batch.draw(textureBackground, 0, 0);
+        imageBackground.draw(this.batch, 1);
     }
 
     @Override
@@ -550,7 +601,10 @@ public class ScreenSurvive implements Screen {
 
     @Override
     public void dispose() {
-        textureBackground.dispose();
+        ((SpriteDrawable) imageBackground.getDrawable()).getSprite().getTexture().dispose();
+        survive.dispose();
+        uiSkin.dispose();
+        uiButton.dispose();
     }
 
     public enum Movement {
@@ -560,16 +614,13 @@ public class ScreenSurvive implements Screen {
     }
     private void initHealthbar() {
 
-        int width =1 ;
+        int width = 1;
         int height = 1;
         Pixmap pixmap = createProceduralPixmap(width, height,0,1,0);
         Pixmap pixmap2 = createProceduralPixmap(width, height,1,0,0);
 
         healthbarForeGround = new Texture(pixmap);
         healthbarBackGround = new Texture(pixmap2);
-
-
-
     }
 
 
